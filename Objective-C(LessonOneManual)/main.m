6ce9bafbd4df4d025074046991d844ca90f7b7d4
Alexander Andrianov <tracktouch@gmail.com>
2021-06-18 17:04:56 +0300

    Initial Commit

---
+++ Objective-C(LessonOneManual)/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  Objective-C(LessonOneManual)
+//
+//  Created by Alexander Andrianov on 18.06.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+  @autoreleasepool {
+    NSLog(@"Start");
+  }
+  return 0;
+}
