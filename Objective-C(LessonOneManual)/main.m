a8035479ab498df63d817e6e74b5422e078048de
Alexander Andrianov <tracktouch@gmail.com>
2021-06-18 19:45:33 +0300

    Console input & output

--- Objective-C(LessonOneManual)/main.m
+++ Objective-C(LessonOneManual)/main.m
@@ -9,7 +9,35 @@
 
 int main(int argc, const char * argv[]) {
   @autoreleasepool {
-    NSLog(@"Start");
+
+    int firstNumber;
+    int secondNumber;
+
+    printf("Enter numbers to get remainder.\n");
+    printf("Number to divide: ");
+    scanf("%d", &firstNumber);
+    printf("Divider: ");
+    scanf("%d", &secondNumber);
+    NSLog(@"Remainder after dividing %d by %d is %d",
+          firstNumber,
+          secondNumber,
+          firstNumber % secondNumber);
+
+    int numberOne;
+    int numberTwo;
+    int numberThree;
+    printf("Now enter three numbers to get middle number for them. \n");
+    printf("First number: ");
+    scanf("%d", &numberOne);
+    printf("Second number: ");
+    scanf("%d", &numberTwo);
+    printf("Third number: ");
+    scanf("%d", &numberThree);
+    NSLog(@"Middle number for %d, %d and %d is %d",
+          numberOne,
+          numberTwo,
+          numberThree,
+          (numberOne + numberTwo + numberThree) / 3);
   }
   return 0;
 }
