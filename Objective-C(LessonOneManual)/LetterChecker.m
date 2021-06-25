c1536524913398c5ddcd60d18bb89b56bfc1ae0f
Alexander Andrianov <tracktouch@gmail.com>
2021-06-25 17:21:56 +0300

    Created letter checker

---
+++ Objective-C(LessonOneManual)/LetterChecker.m
@@ -0,0 +1,19 @@
+//
+//  LetterChecker.m
+//  Objective-C(LessonOneManual)
+//
+//  Created by Alexander Andrianov on 25.06.2021.
+//
+
+#import "LetterChecker.h"
+
+@implementation LetterChecker
+
++ (BOOL) checkIfContainsEnglish: (NSString*)stringValue {
+  NSString *pattern = @"[a-zA-Z]+";
+  NSRegularExpression *regex = [[NSRegularExpression alloc] initWithPattern: pattern options: 0 error: NULL];
+  NSUInteger matches = [regex numberOfMatchesInString: stringValue options: 0 range: NSMakeRange(0, [stringValue length])];
+  return matches > 0;
+}
+
+@end
